787ccf11309ea2848fb0770d8e88f808358efb15
chyen-hhc <chien.hhc1@gmail.com>
2022-10-29 22:07:51 +0700

    .

--- backend/src/addTransaction/addTransaction.controller.ts
+++ backend/src/addTransaction/addTransaction.controller.ts
@@ -8,29 +8,11 @@ const post: ServerRoute = {
   options: {
     description: 'Post add user who paid',
     handler: async (_request: Request, res: ResponseToolkit) => {
-      // const users = await RuleEngineModel.create({ username: username, password: password })
-      //   .then(_data => {
-      //     console.log(_data)
-      //     return _data
-      //   }).catch(
-      //     _err => {console.log("Network error!")
-      //     return {}
-      //   }
-
-      //   )
-
-      //   return res.response({
-      //       data: (users as Array<any>).length != 0
-      //     }).code(201); 
       const { userId, package1, amount } = _request.payload as any;
       await RuleEngineModel
         .create({ userId: userId, package1: package1, amount: amount, state: "false" })
         .then()
         .catch(_err => { console.log("Network error!add transaction controller") })
-
-
-
-
       return res.response({}).code(201);
     },
   }
