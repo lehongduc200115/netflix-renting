f54782b8d263fc22e40577c465e382e020f7e39a
Le Hong Duc <duc.le4@mservice.com.vn>
2022-10-29 18:09:59 +0700

    register fumc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './login/Login'
 import Pricing from './pricing/Pricing'
 import Checkout from './checkout/Checkout'
+import SignUp from './register/SignUp'
 import {
   BrowserRouter as Router,
   Routes,
@@ -28,12 +29,16 @@ class App extends Component {
               <li>
                 <Link to="/checkout">Contact Us</Link>
               </li>
+              <li>
+                <Link to="/register">Sign Up</Link>
+              </li>
             </ul>
            <Routes>
                  {/* <Route exact path='/' element={< Home />}></Route> */}
                  <Route path='/login' element={< Login />}></Route>
                  <Route path='/pricing' element={< Pricing />}></Route>
                  <Route path='/checkout' element={< Checkout />}></Route>
+                 <Route path='/register' element={< SignUp />}></Route>
           </Routes>
           </div>
        </Router>
